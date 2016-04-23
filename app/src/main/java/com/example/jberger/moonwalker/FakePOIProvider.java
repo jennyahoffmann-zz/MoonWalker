2693f84a2202b8d6b7b310bb76c9e15d648fa32a
Jenny Berger <jenny.berger@technloy.com>
2016-04-23 10:38:20 +0200

    chenged coordanitas

--- app/src/main/java/com/example/jberger/moonwalker/FakePOIProvider.java
+++ app/src/main/java/com/example/jberger/moonwalker/FakePOIProvider.java
@@ -13,7 +13,7 @@ public class FakePOIProvider implements GamePOIProvider {
     public List<POI> getLatLng() {
         ArrayList<POI> latLngs = new ArrayList<>();
         latLngs.add(new POI(new LatLng(52.5109273, 13.4107171), "Mission1"));
-        latLngs.add(new POI(new LatLng(52.5129912, 13.4178747), "Mission2"));
+        latLngs.add(new POI(new LatLng(52.5079723, 13.4217567), "Mission2"));
         return latLngs;
     }
 }
